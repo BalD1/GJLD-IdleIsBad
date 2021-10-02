8ab649c0b2f0c2b7668df1f22cf1b2bcda07236c
Florian <florianhusson@hotmail.fr>
2021-10-03 01:27:03 +0200

    oops

--- Idle Is Bad/Assets/Scripts/Bzzter.cs
+++ Idle Is Bad/Assets/Scripts/Bzzter.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Bzzter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        if(collision.tag.Equals("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            player.Death();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
